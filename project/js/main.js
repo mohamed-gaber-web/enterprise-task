59ee9f652c7e46d1c6c1ccd240c8b69bae9ba063
mohamed gaber <mgaberweb@gmail.com>
2023-07-13 01:49:56 +0300

    add some sections

--- project/js/main.js
+++ project/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     loop: true,
     margin: 10,
     nav: true,
+    dots: false,
+
     responsive: {
       0: {
         items: 1,
@@ -11,8 +13,40 @@ $(document).ready(function () {
         items: 3,
       },
       1000: {
-        items: 5,
+        items: 4,
       },
     },
   });
+  var owl = $(".owl-carousel");
+  owl.owlCarousel();
+  $(".next-btn").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+  $(".prev-btn").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+
+  // menu
+  $("#openMenu").click(function () {
+    var menu = $("#menu");
+    if ($(menu).is(":visible")) {
+      $(menu).animate({ width: 0 }, 1000, function () {
+        $(menu).hide();
+      });
+    } else {
+      $(menu).show().animate({ width: 400 }, 500);
+    }
+  });
 });
+
+// Menu
+// const menuToggle = document.getElementById("menu-toggle");
+
+// function buttonclick() {
+//   var menuList = document.getElementById("menu-mobile");
+//   if (menuList.className == "menuOff") {
+//     menuList.className = "menu-mobilemenuOn";
+//   } else {
+//     menuList.className = "menuOff";
+//   }
+// }
