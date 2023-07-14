c8a7fd25cf80109407b8e5919d5e8013f63f3d9d
mohamed gaber <mgaberweb@gmail.com>
2023-07-15 01:23:27 +0300

    finished task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var sass = require("gulp-sass")(require("sass"));
+// var gulp = require("gulp");
+// var concat = require("gulp-concat");
+// var sass = require("gulp-sass")(require("sass"));
+// var minify = require("gulp-minify");
+// var imagemin = require("gulp-imagemin");
+
+import gulp from "gulp";
+import minifyImage from "gulp-imagemin";
+import sass from "gulp-sass";
+import minify from "gulp-minify";
 
 gulp.task("styles", function () {
   return gulp
@@ -14,12 +21,21 @@ gulp.task("scripts", function () {
   return (
     gulp
       .src("project/js/*.js")
+      .pipe(minify())
       // .pipe(concat("main.css"))
       .pipe(gulp.dest("dist/js"))
   );
 });
 
+gulp.task("images", function () {
+  return gulp
+    .src("project/images/*")
+    .pipe(minifyImage())
+    .pipe(gulp.dest("dist/images"));
+});
+
 gulp.task("watch", function () {
+  gulp.watch("project/images/*", gulp.series("images"));
   gulp.watch("project/css/**/*.scss", gulp.series("styles"));
   gulp.watch("project/js/**/*.js", gulp.series("scripts"));
 });
--- project/js/main.js
+++ project/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
+    items: 4,
     nav: true,
     dots: false,
 
@@ -10,9 +11,9 @@ $(document).ready(function () {
         items: 1,
       },
       600: {
-        items: 3,
+        items: 1,
       },
-      1000: {
+      1600: {
         items: 4,
       },
     },
@@ -26,7 +27,99 @@ $(document).ready(function () {
     owl.trigger("prev.owl.carousel");
   });
 
-  // menu
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    // slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    asNavFor: ".slider-nav",
+    prevArrow: false,
+    nextArrow: false,
+    verticalSwiping: true,
+    vertical: true,
+    // centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 5,
+    // slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    // centerMode: true,
+    vertical: true,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  $(window).on("resize orientationchange", function () {
+    if ($(window).width() > 1200) {
+      $(".slider-nav").slick("unslick");
+      $(".slider-nav").slick({
+        slidesToShow: 5,
+        asNavFor: ".slider-for",
+        vertical: true,
+        focusOnSelect: true,
+        autoplay: false,
+        centerMode: true,
+        prevArrow: false,
+        nextArrow: false,
+      });
+    }
+  });
+
+  // Menu Toggle
   $("#openMenu").click(function () {
     var menu = $("#menu");
     if ($(menu).is(":visible")) {
@@ -38,15 +131,3 @@ $(document).ready(function () {
     }
   });
 });
-
-// Menu
-// const menuToggle = document.getElementById("menu-toggle");
-
-// function buttonclick() {
-//   var menuList = document.getElementById("menu-mobile");
-//   if (menuList.className == "menuOff") {
-//     menuList.className = "menu-mobilemenuOn";
-//   } else {
-//     menuList.className = "menuOff";
-//   }
-// }
